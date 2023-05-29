59b9e884d391b5060ea9816539352dd7c6854547
cbroussey <131813127+cbroussey@users.noreply.github.com>
2023-05-30 01:01:22 +0200

    Mise à jour et réglage de bugs
    Le site a meilleure allure en général et plein de bugs ont été fixés

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const titles = ["Colin Broussey", "Projets", "CV", "Contact"]
+
+const animSpeed = 510;
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function main() {
     // document.getElementById("wave").setAttribute("viewBox", `0 0 1440 ${Math.ceil(60*window.innerHeight/100)}`); 
-    window.currentTab = 0;
+    window.currentTab = 1;
     window.tabs = document.getElementsByClassName("tab")
     tabs[0].style.backgroundColor = "rgba(255, 255, 255, 0.3)"
     for(let i = 0; i < tabs.length; i++) {
@@ -28,6 +32,7 @@ function main() {
         setTimeout(blink, delay)
     }
     blink()
+    setTab(0)
 }
 
 async function write(text, speed) {
@@ -37,23 +42,56 @@ async function write(text, speed) {
         setTimeout(function(){ write(text.slice(1), speed) }, speed)
     } else {
         cmd.children[cmd.children.length - 1].innerHTML = cmd.children[cmd.children.length - 1].innerHTML.slice(0, -1)
-        sleep(100).then(() => {
+        setTimeout(() => {
             document.getElementById("cmd").innerHTML += "<li>colin.broussey@portfolio ~ $ _</li>"
             document.getElementById("cmd").scrollTop = document.getElementById("cmd").scrollHeight
             // if(document.getElementById("cmd").children.length > 10) document.getElementById("cmd").replaceChildren(...Array.from(document.getElementById("cmd").querySelectorAll("li")).slice(1))
             window.blink = true
             window.writing = false
-        })
+        }, 100)
     }
 }
 
 async function setTab(tab) {
     if(tab != window.currentTab && !window.writing) {
         window.tabs[window.currentTab].style.backgroundColor = ""
-        window.currentTab = tab
-        window.tabs[window.currentTab].style.backgroundColor = "rgba(255, 255, 255, 0.3)"
+        // let titre = document.querySelector(`#page${window.currentTab} > h1`)
+        let titre = document.querySelector("#wavecontainer > h1")
+        window.tabs[tab].style.backgroundColor = "rgba(255, 255, 255, 0.3)"
         window.blink = false
         cmd = document.getElementById("cmd")
+        // let contenu = await fetch(`${titles[tab]}.html`)
+        titre.classList.remove("enterLeft")
+        void titre.offsetWidth;
+        titre.classList.add("exitLeft")
+        let lines = document.querySelectorAll(`#page${tab} > *:not(h1)`)
+        async function animate(i = 0) {
+            if (i < lines.length) {
+                // console.log(lines[i])
+                // document.querySelectorAll("body > *")[i].style.animation = "slide-in .8s ease-in-out both"
+                lines[i].style.animation = "slide-in .8s ease-in-out both"
+                setTimeout(function(){ animate(i + 1) }, 50)
+            }
+        }
+        setTimeout(() => {
+            document.querySelector(`#page${window.currentTab}`).style.display = "none"
+            titre.innerHTML = `${titles[tab]}`
+            if (titles[tab].length > 8) titre.style.top = "0.7em"
+            else titre.style.top = "1.2em"
+            // let titre = document.querySelector(`#page${tab} > h1`)
+            titre.classList.remove("exitLeft")
+            void titre.offsetWidth;
+            titre.classList.add("enterLeft")
+            document.querySelector(`#page${tab}`).style.display = "flex"
+            animate();
+            window.currentTab = tab
+        }, animSpeed)
         await write(`cd ${window.tabs[tab].children[0].innerHTML}`, 30)
     }
 }
+
+function adapt() {
+    document.getElementById("cmd").scrollTop = document.getElementById("cmd").scrollHeight
+}
+
+window.addEventListener("resize", adapt);
